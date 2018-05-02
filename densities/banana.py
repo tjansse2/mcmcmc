0bf1a80cc20beb568463db4fc122e105fcc8d848
Timo Janssen <timo.janssen@theorie.physik.uni-goettingen.de>
2018-05-02 19:09:30 +0200

    finish HMC banana example

--- densities/banana.py
+++ densities/banana.py
@@ -17,6 +17,18 @@ class Banana(Density):
         phi[1] = phi[1] + self.b*phi[0]**2 - 100*self.b
 
         return multivariate_normal.pdf(phi, mean=np.zeros(ndim), cov=[100]+(ndim-1)*[1])
+        #return np.sqrt((2*np.pi)**ndim * 100) * np.exp(-.5*(phi[0]**2/100 + np.sum(phi[1:]**2)))
+
+    def log_pdf(self, x):
+        if not type(x) is np.ndarray:
+            x = np.array(x)
+
+        ndim = x.size
+
+        phi = copy(x)
+        phi[1] = phi[1] + self.b*phi[0]**2 - 100*self.b
+
+        return -np.log(np.sqrt((2*np.pi)**ndim * 100)) - .5*(phi[0]**2/100 + np.sum(phi[1:]**2))
 
     def log_pdf_gradient(self, x):
         if not type(x) is np.ndarray:
@@ -25,7 +37,7 @@ class Banana(Density):
         ndim = x.size
 
         if ndim==2:
-            return np.array([-100*x[0]-2*self.b*x[0]*(x[1]+self.bb*x[1]**2-100*self.b), 100*self.b-x[1]-self.b*x[1]**2])
+            return np.array([-1/100*x[0]-2*self.b*x[0]*(x[1]+self.b*x[0]**2-100*self.b), 100*self.b-x[1]-self.b*x[0]**2])
 
         else:
             return NotImplementedError()
--- examples/hmc_banana.py
+++ examples/hmc_banana.py
@@ -3,8 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from densities.banana import Banana
-from samplers.mcmc.metropolis import StaticMetropolis
-from proposals.gaussian import Gaussian
+from samplers.mcmc.hmc import StaticHMC
 
 np.random.seed(1234)
 
@@ -16,12 +15,11 @@ nsamples = 100
 #start = np.array([0., 10.])
 start = np.array([-20., -30.])
 
-mu = np.array([0., 0.])
-cov = np.array([[10., 0.], [0., 10.]])
-proposal = Gaussian(mu=mu, cov=cov)
-sampler = StaticMetropolis(ndim, target.pdf, proposal)
+stepsize = .3
+nsteps = 50
+sampler = StaticHMC(ndim, target.pdf, target.log_pdf_gradient, stepsize, stepsize, nsteps, nsteps)
 
-samples, mean, variance = sampler.sample(nsamples, start)
+samples = sampler.sample(nsamples, start)
 
 n_accepted = 1
 for i in range(1, nsamples):
@@ -29,7 +27,6 @@ for i in range(1, nsamples):
             n_accepted += 1
 
 print('Acceptance rate:', n_accepted/nsamples)
-plt.scatter(samples[:, 0], samples[:, 1], 1, 'red')
 
 
 a = np.sqrt(599)
@@ -39,10 +36,33 @@ ellipse = b/a*np.sqrt(a**2-x**2)
 contour1 = ellipse - bananicity*x**2 + 100*bananicity
 contour2 = -ellipse - bananicity*x**2 + 100*bananicity
 
-#plt.contour(X, Y, f)
+plt.figure(1)
+plt.scatter(samples[:, 0], samples[:, 1], 1, 'red')
 plt.plot(x, contour1, 'b-')
 plt.plot(x, contour2, 'b-')
 plt.xlim([-50, 50])
 plt.ylim([-60, 20])
+
+#plt.figure(2)
+#plt.xlim([-50, 50])
+#plt.ylim([-60, 20])
+#x = np.linspace(-50, 50, 100)
+#y = np.linspace(-60, 20, 100)
+#X, Y = np.meshgrid(x, y)
+#logf = np.zeros([100, 100])
+#U = np.zeros([100, 100])
+#V = np.zeros([100, 100])
+#for i, x_i in enumerate(x):
+#    for j, y_j in enumerate(y):
+#        logf[j, i] = target.log_pdf(np.array([x_i, y_j]))
+#        grad = target.log_pdf_gradient(np.array([x_i, y_j]))
+#        U[j, i] = grad[0]
+#        V[j, i] = grad[1]
+#
+#levels = np.linspace(-1000, 0, 7)
+#plt.contourf(X, Y, logf, levels=levels)
+#plt.colorbar()
+#plt.quiver(X[::3, ::3], Y[::3, ::3], U[::3, ::3], V[::3, ::3])
+
 plt.show()
 
