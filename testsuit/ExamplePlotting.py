476e15b0899715528d6f7ba053023db602a4ccf0
MicMa <michael.mahn@tu-dortmund.de>
2018-08-16 15:38:44 +0200

    testsuit more efficient

--- testsuit/ExamplePlotting.py
+++ testsuit/ExamplePlotting.py
@@ -6,17 +6,23 @@ from plot.viewer import view_parm2D
 # Load Data
 
 gauss = np.load("Results/gauss.npy")
+hmc = np.load("Results/hmc.npy")
+
 
 # plot 1D
 
-f, (axes) = plt.subplots(2, 3 ,figsize=(15,10))
+
+lag = 0 # no influence on effective_sample_size ( not yet implemented)
+center = 0.333
+width = 0.005
+
+f, (axes) = plt.subplots(2, 3, sharey=True,figsize=(15,10))
 axes = np.reshape(axes,6)
 
-lag = 0 # no influence on effectiv sample size ( not yet implemented )
 for i,center in enumerate(np.unique(gauss["center"])):
     for width in np.unique(gauss["width"]):
         y,x = extract(gauss,"effective_sample_size","beta",varFixed=[("center",center),("width",width),("virtual_lag",lag)])
-        axes[i].plot(x,y[:,0],label="cov "+str(width))
+        axes[i].plot(x,y[:,0],label="width "+str(width))
         axes[i].set_title("gauss - c: "+str(center))
 handles, labels = axes[0].get_legend_handles_labels()
 f.legend(handles, labels, loc=7)
@@ -24,6 +30,32 @@ plt.suptitle("effective sample size")
 plt.show()
 
 
+
+center = 0.333
+w = 0.005
+lag = 0 # no influence on effectiv sampling size ( not yet implemented )
+mass = 1.1
+step_size = -1
+steps = -1
+
+f, (axes) = plt.subplots(4, 3, sharey=True,figsize=(15,15))
+axes = np.reshape(axes,12)
+
+for i,step_size in enumerate(np.unique(hmc["step_size"])):
+    for steps in np.unique(hmc["steps"]):
+        if i == 9:
+            i=10
+        y,x = extract(hmc,"effective_sample_size","beta",varFixed=[("center",0.333),("width",0.005),("virtual_lag",lag),("steps",steps),("step_size",step_size),("mass",0.5)])
+        axes[i].plot(x,y[:,0],label="steps "+str(steps))
+        axes[i].set_title("hmc - c: "+str(center)+ " - w: "+str(w)+" - l: "+str(lag)+" - m: "+str(mass)+ " - ss: "+str(step_size))
+
+handles, labels = axes[0].get_legend_handles_labels()
+f.legend(handles, labels, loc=7)
+plt.suptitle("effective sample size")
+plt.show()
+
 # view 2D
-view_parm2D(gauss,"ks_pvalue","beta","virtual_lag",varFixed=[("center",0.33),("width",0.5)],nameZ="p_value",critical=0.05,title="gauss - ks")
+view_parm2D(gauss,"ks_pvalue","beta","virtual_lag",varFixed=[("center",0.333),("width",0.005)],nameZ="p_value",critical=0.05,title="gauss - ks")
+
+view_parm2D(hmc,"ks_pvalue","beta","virtual_lag",varFixed=[("center",0.333),("width",0.005),("mass",1.1),("steps",15),("step_size",0.119)],nameZ="p_value",critical=0.05,title="hmc - ks")
 
--- testsuit/Test.py
+++ testsuit/Test.py
@@ -7,6 +7,14 @@ from sys import stderr
 from scipy.stats import ks_2samp
 from stats import *
 
+try:
+    from tqdm import tqdm
+except ImportError:
+     print("tqdm not installed")
+     def tqdm(whatever,*args,**kwargs):
+         return whatever
+
+
 
 
 ################################
@@ -38,20 +46,13 @@ class TEST: #Test Environment for Sampling algoriThms
         else:
             self.fixedsize=None
 
-
-        self.nSamples=len(self.Samples)
-        self.nVSamples=self.nSamples
-
-
-
-
         if len(np.shape(self.Reference["sample"])) == 1:
             self.ndim = 1
         else:
             self.ndim = np.shape(self.Reference["sample"])[1]
 
-        self.virtual_lag = False
-        self.apply_lag(lag)
+        self.nSamples=len(self.Samples)
+        self.applyLag(lag)
 
     
     def set_interface(self,name):
@@ -61,82 +62,90 @@ class TEST: #Test Environment for Sampling algoriThms
                 mod = getattr(mod, comp)
             self.interface = mod
 
-    def apply_lag(self,lag):
+    def applyLag(self,lag):
         if hasattr(lag, "__len__"):
             self.lag = lag
             if 0 not in lag:
                 self.lag.append(0)
-            else:
-                lag.remove(0)
+
             nolag = np.zeros(self.nSamples)
             self.Samples = append_field(self.Samples,nolag,"virtual_lag","int64")
-            temp = np.empty(len(lag)*self.nSamples,dtype=self.Samples.dtype)
-            index = 0
-            for sample in self.Samples:
-                for l in lag:
-                    temp[index]=sample
-                    temp[index]["virtual_lag"]=l
-                    index +=1
-            self.Samples = np.append(self.Samples,temp)
-            self.virtual_lag = True
-            self.nVSamples=len(self.Samples)
+
+            temp = np.empty(len(self.lag),dtype=np.ndarray)
+            for i in range(len(temp)):
+                temp[i]=self.Samples.copy()
+                if i != 0:
+                    temp[i]["virtual_lag"] = np.ones(self.nSamples)*self.lag[i]
 
 
+            self.Samples = temp
+            self.nlags = len(self.lag)
         else:
-            self.lag=lag
+            self.lag=[lag]
+            self.Samples = [self.Samples]
+            self.nlags=1
 
 
-    # param Limits : list of ints, calculates fraction of sequences with minimum lengths given
-    def sequenceLength(self,Limits=[10,25,35,50]):
-        seql = []
-        seqlf = []
-        for sample in self.Samples:
-            if self.virtual_lag==True:
-                lag = sample["virtual_lag"]+1
-            else:
-                lag = self.lag+1
-            data = self.interface.load(sample["sample"])[::lag][:self.fixedsize]
-            seql.append(sequenceLength(data))
-            seqlf.append(seqLFraction(seql[-1],Limits))
-        self.Samples = append_field(self.Samples,seql,"sequenceLength","O")
-        self.Samples = append_field(self.Samples,seqlf,"sequenceLengthFraction","O")
+    def KolmogorowSmirnow(self,data):
+        p_axis = []
+        for axis in range(self.ndim):
+            p_axis.append(ks_2samp(self.Reference["sample"][:,axis],data[:,axis])[1])
+        return p_axis
 
-    def  KolmogorowSmirnow(self):
-        ks_pvalue = np.empty([self.nVSamples,self.ndim])
-        for i,sample in enumerate(self.Samples):
-            if self.virtual_lag==True:
-                lag = sample["virtual_lag"]+1
-            else:
-                lag = self.lag+1
-            data = self.interface.load(sample["sample"])[::lag][:self.fixedsize]
-            for axis in range(self.ndim):
-                ks_pvalue[i,axis] = ks_2samp(self.Reference["sample"][:,axis],data[:,axis])[1]
-        self.Samples = append_field(self.Samples,ks_pvalue.tolist(),"ks_pvalue","O")
+    def chiq(self,data):
+        bined_data,e = np.histogramdd(data)
+        bined_ref ,e = np.histogramdd(self.Reference["sample"],bins=e)
+        # norm reference
+        bined_ref *= np.sum(bined_data)/np.sum(bined_ref)
+        return chiq(bined_ref,bined_data)[1] 
 
-    def chiq(self):
-        chiq_pvalue = np.empty(self.nVSamples)
-        for i,sample in enumerate(self.Samples):
-            if self.virtual_lag==True:
-                lag = sample["virtual_lag"]+1
-            else:
-                lag = self.lag+1
-            data = self.interface.load(sample["sample"])[::lag][:self.fixedsize]
-            bined_data,e = np.histogramdd(data)
-            bined_ref ,e = np.histogramdd(self.Reference["sample"],bins=e)
-            bined_ref *=np.sum(bined_data)/np.sum(bined_ref)
-            chiq_pvalue[i] = chiq(bined_ref,bined_data)[1] 
-        self.Samples = append_field(self.Samples,chiq_pvalue.tolist(),"chiq_pvalue","float64")
+    def startTesting(self,chiq=True,ks=True,seqLength=True, Limits=[10,25,35,50]):
+        if chiq:
+            chiq_pvalue = np.empty([self.nSamples,self.nlags])
+        if ks:
+            ks_pvalue = np.empty([self.nSamples,self.nlags],dtype=np.ndarray)
+        if seqLength:
+            seql = np.empty([self.nSamples,self.nlags],dtype=list)
+            seqlf = np.empty([self.nSamples,self.nlags],dtype=list)
+            seqlfLimits = np.empty([self.nSamples,self.nlags],dtype=list)
 
 
-    def save(self,filename):
-        np.save(filename,self.Samples)
+        for i_sample,sample_path in tqdm(enumerate(self.Samples[0]["sample"]),total=self.nSamples):
+            for i_lag, lag in enumerate(self.lag):
+                data = self.interface.load(sample_path)[::lag+1][:self.fixedsize]
+
+                if chiq:
+                    chiq_pvalue[i_sample,i_lag] = self.chiq(data)
+
+                if ks:
+                    ks_pvalue[i_sample,i_lag] = self.KolmogorowSmirnow(data)
+
+                if seqLength:
+                    seql[i_sample,i_lag] = sequenceLength(data)
+                    if Limits is not None:
+                        seqlf[i_sample,i_lag] = seqLFraction(seql[i_sample,i_lag],Limits)
+                        seqlfLimits[i_sample,i_lag] = Limits
+
+        for i_lag in range(self.nlags):
+            if chiq:
+                self.Samples[i_lag] = append_field(self.Samples[i_lag],chiq_pvalue[:,i_lag],"chiq_pvalue",dtype=float)
+            if ks:
+                self.Samples[i_lag] = append_field(self.Samples[i_lag],ks_pvalue[:,i_lag],"ks_pvalue",dtype=list)
+            if seqLength:
+                self.Samples[i_lag] = append_field(self.Samples[i_lag],seql[:,i_lag],"sequenceLength",dtype=list)
+                if Limits is not None:
+                    self.Samples[i_lag] = append_field(self.Samples[i_lag],seqlf[:,i_lag],"seqLFraction",dtype=list)
+                    self.Samples[i_lag] = append_field(self.Samples[i_lag],seqlfLimits[:,i_lag],"seqLFractionLimits",dtype=list)
 
 
 
 
 
-
-
+    def write(self,filename):
+        temp = self.Samples[0]
+        for sample in self.Samples[1:]:
+            temp = np.append(temp,sample) 
+        np.save(filename,temp)
 
 
 def append_field(recarray, newFild, name, dtype):
